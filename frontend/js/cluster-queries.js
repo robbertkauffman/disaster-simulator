7bab84c940f4bfe6492129cbf50e50bd13e6b202
Robbert Kauffman <robbertkauffman@gmail.com>
2022-03-16 10:03:28 -0400

    button interactions

--- frontend/js/cluster-queries.js
+++ frontend/js/cluster-queries.js
@@ -3,6 +3,8 @@ REGION_A_ENDPOINT = "http://127.0.0.1:5001";
 // REGION_B_ENDPOINT = "http://ec2-54-71-127-55.us-west-2.compute.amazonaws.com:5000";
 REGION_B_ENDPOINT = "http://127.0.0.1:5002";
 // REGION_C_ENDPOINT = "http://127.0.0.1:5003";
+REGION_A_NAME = "US_EAST_2";
+REGION_B_NAME = "US_WEST_1";
 FIND_PATH = "find";
 INSERT_PATH = "insert";
 SEARCH_PATH = "search";
@@ -10,13 +12,18 @@ REGION_A_CONTAINER = "region-a-container";
 REGION_B_CONTAINER = "region-b-container";
 // REGION_C_CONTAINER = "region-c-container";
 
-CLUSTER_DETAILS_API = "https://data.mongodb-api.com/app/t9-realm-svcs-qxpsp/endpoint/getClusterDetails";
+REALM_BASE_URL = "https://data.mongodb-api.com/app/t9-realm-svcs-qxpsp/endpoint";
+CLUSTER_DETAILS_PATH = "/getClusterDetails";
+TEST_FAILOVER_PATH = "/testFailover";
+ATLAS_PROCESSES_PATH = "/atlasProcesses";
+CREATE_NETWORK_PARTITION_PATH = "/createNetworkPartition";
 
 // do queries each second
 setInterval(queryAllRegions, 1000);
 
 drawTopologyLines()
 fetchClusterDetails();
+setAppRegions();
 
 function queryAllRegions() {
   find(REGION_A_ENDPOINT, REGION_A_CONTAINER);
@@ -134,21 +141,84 @@ function drawTopologyLines() {
 }
 
 async function fetchClusterDetails() {  
-  clusterDetails = await fetch(CLUSTER_DETAILS_API)
+  clusterDetails = await fetch(REALM_BASE_URL + CLUSTER_DETAILS_PATH)
     .then(response => response.json())
     .then(data => {
       return data;
   }).catch(
-    e => console.log(e)
+    e => console.log(`Fetching cluster details failed: ${e}`)
   );
   
   if (clusterDetails && clusterDetails.replicationSpec) {
     Object.keys(clusterDetails.replicationSpec).forEach((key, i) => {
-      elm = document.getElementById("mongo-" + (i + 1) + "-caption");
-      if (elm) {
-        currCaption = elm.innerHTML;
-        elm.innerHTML = currCaption.replace("{REGION}", key);
-      }
+      replaceRegionCaption("mongo-" + (i + 1) + "-caption", key);
     });
+  }
+}
+
+function setAppRegions() {
+  replaceRegionCaption("app-a-caption", REGION_A_NAME);
+  replaceRegionCaption("app-b-caption", REGION_B_NAME);
+  replaceRegionCaption("region-a-log-header", REGION_A_NAME);
+  replaceRegionCaption("region-b-log-header", REGION_B_NAME);
+}
+
+function replaceRegionCaption(htmlId, region) {
+  elm = document.getElementById(htmlId);
+  if (elm) {
+    currCaption = elm.innerHTML;
+    elm.innerHTML = currCaption.replace("{REGION}", region);
+  }
+}
+
+async function testFailover() {  
+  clusterDetails = await fetch(REALM_BASE_URL + TEST_FAILOVER_PATH, { method: 'POST' })
+    .then(response => response.text())
+    .then(data => {
+      console.log(data);
+      buttonElm = document.getElementById("test-failover-button");
+      if (buttonElm) {
+        buttonElm.disabled = true;
+        buttonElm.innerHTML = "Initiated regional failover...";
+      }
+      setTimeout(() => enableTestFailover(buttonElm), 600000);
+  }).catch(
+    e => console.log(`Test failover failed: ${e}`)
+  );
+}
+
+function enableTestFailover(buttonElm) {
+  buttonElm.disabled = false;
+  buttonElm.innerHTML = "Atlas regional failover";
+}
+
+async function createNetworkPartition() {
+  clusterDetails = await fetch(REALM_BASE_URL + TEST_FAILOVER_PATH, { method: 'POST' })
+  .then(response => response.text())
+  .then(data => {
+    console.log(data);
+    buttonElm = document.getElementById("network-partition-button");
+    if (buttonElm) {
+      buttonElm.disabled = true;
+      if (buttonElm.className.indexOf("create-partition") !== -1) {
+        buttonElm.innerHTML = "Creating network partition...";
+      } else {
+        buttonElm.innerHTML = "Restoring network connectivity...";
+      }
+    }
+    setTimeout(() => enableCreateNetworkPartition(buttonElm), 2000);
+  }).catch(
+    e => console.log(`Create/restore network partition failed: ${e}`)
+  );
+}
+
+function enableCreateNetworkPartition(buttonElm) {
+  buttonElm.disabled = false;
+  if (buttonElm.className.indexOf("create-partition") !== -1) {
+    buttonElm.innerHTML = "Restore network partition";
+    buttonElm.className = buttonElm.className.replace("create-partition", "restore-partition")
+  } else {
+    buttonElm.innerHTML = "Create network connectivity";
+    buttonElm.className = buttonElm.className.replace("restore-partition", "create-partition")
   }
 }

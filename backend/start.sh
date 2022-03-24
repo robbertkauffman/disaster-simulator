808f8e3c0f1c072a0b8b4a74c5a8bcea7e68aa25
Robbert Kauffman <robbertkauffman@gmail.com>
2022-03-24 15:00:35 -0400

    updated backend code to match what was deployed on VMs

---
+++ backend/start.sh
@@ -0,0 +1,4 @@
+nohup python3 backend_with_NO_retry.py &
+nohup python3 backend_with_retry_reads.py &
+nohup python3 backend_with_retry_reads_writes.py &
+nohup python3 backend_with_retry_writes.py &
